0c5c610262c4aadaec8c3064a2527269626c46c8
kirk <kirk91.han@gmail.com>
2017-02-16 11:04:11 +0800

    exclude vcs

--- build_pkg.sh
+++ build_pkg.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-date=`date +"%Y%m%d_%H%M%S"`
 echo ${TRAVIS_TAG}
-tar -zcvf ucenter-${TRAVIS_TAG}.tar.gz --exclude=.git/ .
+tar -zcvf ucenter-${TRAVIS_TAG}.tar.gz --exclude="./.git" --exclude="./.gitignore" *
