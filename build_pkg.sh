ec14514bc099f5e3445916018e997acca4066649
kirk <kirk91.han@gmail.com>
2017-02-16 11:19:39 +0800

    exclude more files

--- build_pkg.sh
+++ build_pkg.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 echo ${TRAVIS_TAG}
-tar -zcvf ucenter-${TRAVIS_TAG}.tar.gz --exclude="./.git" --exclude="./.gitignore" *
+tar -zcvf ucenter-${TRAVIS_TAG}.tar.gz --exclude="./.git" --exclude="./.gitignore" --exclude="__pycache__" --exclude=".DS_Store" *
